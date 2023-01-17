955b58858ad28dd114347f2a0843999703a6cbbe
Tian Hoe-business <n.tianhoe@gmail.com>
2023-01-18 07:47:17 +0800

    updated database

--- .venv/web_project/settings.py
+++ .venv/web_project/settings.py
@@ -90,8 +90,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
     }
 }
 
