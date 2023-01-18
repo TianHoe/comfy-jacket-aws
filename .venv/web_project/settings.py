7951c2236109d3fcf4bde2e044db15e3f7f0e06c
Tian Hoe-business <n.tianhoe@gmail.com>
2023-01-18 19:17:52 +0800

    update available port

--- .venv/web_project/settings.py
+++ .venv/web_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&0@fe^8((=p&xcbik@6o6mu$fkz36rwxfnccsawx5k7d35qczd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.226.152.33', ]
+ALLOWED_HOSTS = ['18.208.127.94', ]
 
 
 # Application definition
